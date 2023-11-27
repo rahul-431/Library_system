1c18a20d559385a902ab4e9c5d8f0652fa8582c5
Rahul Mijar <rahul.mijar@trunary.com>
2023-11-27 15:04:37 +0545

    Controller: ManageBook is added

---
+++ Controller/ManageBook.cs
@@ -0,0 +1,23 @@
+using System;
+namespace Library_system{
+    class ManageBook{
+            Book book=new Book();
+        public void addNewBook(string bookId,string author, string title,string price, bool avail)
+        {
+
+            book.bookId=bookId;
+            book.author=author;
+            book.title=title;
+            book.price=price;
+            book.available=avail;   
+        }
+        public void removeBook(string bookId)
+        {
+
+        }
+        public string getAllBooks()
+        {
+            return "Book id: "+book.bookId+" Title: "+book.title+" Author: "+book.author+" Price: "+book.price;
+        }
+    }
+}
